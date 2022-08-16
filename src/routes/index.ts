e663dca9c863ed2e403af46e6e82d58df95ce3f3
LAPTOP-VL3C11E4\Ayoub <sabiriayoub@gmail.com>
2022-08-16 14:16:59 +0200

    http get method

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,13 @@
     -return it as data in place of the empty array */
 
 export async function get() {
+	const url = "https://jsonkeeper.com/b/OHVP";
+	const response = await fetch(url);
+	const result = await response.json();
 	return {
 		status: 200,
 		body: {
-			data: [] //return data here!
+			data: result
 		}
 	};
 }

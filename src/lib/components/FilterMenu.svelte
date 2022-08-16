ae15a5b855fd30bc69bbd427bd555ace3e0c5da1
LAPTOP-VL3C11E4\Ayoub <sabiriayoub@gmail.com>
2022-08-16 15:11:55 +0200

    Filter Component

---
+++ src/lib/components/FilterMenu.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    export let countries : string[];
+    export let selectedCountry : string;
+</script>
+
+<div class="flex justify-center">
+    <div class="mb-3 xl:w-96">
+      <select class="form-select appearance-none
+        block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding bg-no-repeat
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example"
+        bind:value={selectedCountry}
+        >
+          <option selected>Select a Country</option>
+            {#each countries as country }
+                <option value= {country}>{country}</option>
+            {/each}
+      </select>
+    </div>
+  </div>
